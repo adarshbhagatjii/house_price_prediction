55662ec169834582140da98c0480c3ce939d3546
Adarsh Bhagat <147369374+adarshbhagatjii@users.noreply.github.com>
2026-03-06 19:30:33 +0530

    update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ with open('rf_model.joblib', 'rb') as file:
     model = joblib.load(file)
 
 df=pd.read_csv('cleaned_df.csv')
-st.title("House Price Prediction App")
+st.title("🏠 House Price Prediction App")
 
 with st.sidebar:
-    st.title('house price prediction')
+    st.title('🏠 house price prediction')
     st.image('houseimage.jpg', width=300)
 
 def encoded_loc(location):
@@ -26,10 +26,10 @@ with st.container(border=True):
     
     with col1:
         location = st.selectbox("Location", df['location'].unique())
-        sqft = st.number_input("Square Footage", min_value=300, step=10)
+        sqft = st.number_input("Square Foot", min_value=300, step=10)
         
     with col2:
-        bhk = st.selectbox("BHK", options=df['bhk'].unique())
+        bhk = st.selectbox("BHK", options=sorted(df['bhk'].unique()))
 
         bath = st.selectbox("Bathrooms", options=sorted(df['bath'].unique()))
 
@@ -42,4 +42,5 @@ if c2.button("Predict Price"):
         pred=model.predict(inp_data)
         pred=float(f'{pred[0]:.2f}')
         st.success(f"The predicted price of the house is: ₹{pred*100000}")
+
 
